a6eab1f7143b183cec60bfa3606bf7ff9c85a12a
waleed elkhonany <waleedelkhonany@gmail.com>
2025-04-11 18:46:48 +0200

    Dashboard

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+    // return view('layouts.main');
 });
 
 Route::get('/dashboard', function () {
